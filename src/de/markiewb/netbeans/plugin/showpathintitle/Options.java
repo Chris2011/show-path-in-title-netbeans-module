883c1dd9a59ca6778a894d6737958bcfe505838f
benno markiewicz <benno.markiewicz@googlemail.com>
2012-04-15 00:28:44 +0200

    Prepare for options support

---
+++ src/de/markiewb/netbeans/plugin/showpathintitle/Options.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.markiewb.netbeans.plugin.showpathintitle;
+
+/**
+ *
+ * @author Bender
+ */
+class Options {
+
+    boolean showProjectName = true;
+    boolean showFileName = true;
+    boolean showIDEVersion = true;
+}
