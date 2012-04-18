44b46e7d5bdaf650d1a380045f01195782814672
benno markiewicz <benno.markiewicz@googlemail.com>
2012-04-18 23:01:39 +0200

    [doc] updated some project doc like author name [license] add apache2 licensing file and headers which are required for verification at the plugin portal

--- src/de/markiewb/netbeans/plugin/showpathintitle/ShowPathInTitleOptions.java
+++ src/de/markiewb/netbeans/plugin/showpathintitle/ShowPathInTitleOptions.java
@@ -1,6 +1,17 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright 2012 markiewb
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package de.markiewb.netbeans.plugin.showpathintitle;
 
@@ -8,8 +19,8 @@ import java.util.prefs.Preferences;
 import org.openide.util.NbPreferences;
 
 /**
- * Simple POJO for loading/saving options. Note: No setter/getter were introduced for the ease of usage. It is a plain
- * data holder.
+ * Simple POJO for loading/saving options. Note: No setter/getter were
+ * introduced for the ease of usage. It is a plain data holder.
  *
  * @author markiewb
  */
